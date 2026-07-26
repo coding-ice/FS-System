4d59d02472da22344c72ebcbb8e208622425e00e
ice <1999iceweb@gmail.com>
2026-07-26 11:39:54 +0800

    chore: remove deployment workflow and compliance records
    - Deleted the deploy.yml workflow for VitePress deployment to Aliyun.
    - Removed ICP and PSB compliance records from the documentation.
    - Eliminated related CSS and layout components from the theme.

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,11 +2,9 @@ import DefaultTheme from "vitepress/theme";
 import { Sandbox } from "vitepress-plugin-sandpack";
 import "vitepress-plugin-sandpack/dist/style.css";
 import "./custom.css";
-import Layout from "./Layout.vue";
 
 export default {
   extends: DefaultTheme,
-  Layout,
   enhanceApp(ctx) {
     DefaultTheme.enhanceApp(ctx);
     ctx.app.component("Sandbox", Sandbox);
